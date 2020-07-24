6f6bdbff6485acfe501241d6d854a3f2e363bd29
Daniel <danielhajialigol@gmail.com>
2020-07-23 22:44:40 -0400

    Renamed more columns

--- Modeling/ARIMA/Data_Cleaning/Data_Cleaner.R
+++ Modeling/ARIMA/Data_Cleaning/Data_Cleaner.R
@@ -10,7 +10,8 @@ require(dplyr)
 # Oupt:
   # df_list [list]: List of cleaned data frames before and after COVID-19.
 data_cleaner <- function(data_directory, file_name = "FINAL_PJM_DATA.xlsx",
-                         covid_date = "2020-03-11", beginning_date = "2019-01-01"){
+                         covid_date = "2020-03-11", beginning_date = "2019-01-01",
+                         remove = 'no'){
   
   # Set working directory to data directory
   setwd(data_directory)
@@ -26,10 +27,12 @@ data_cleaner <- function(data_directory, file_name = "FINAL_PJM_DATA.xlsx",
   cleaned_df <- cleaned_df %>%
     rename(Day_Ahead_Hourly_Price = "Day-Ahead Hourly Price(kWh)",
            Real_Time_Hourly_Price = "Real-Time Hourly Price(kWh)",
-           Data_Date = "Data Date",
-           Hour_Number = "Hour Number",
-           Demand_Forecast = "Demand Forecast (MW)",
-           Demand_MW = "Demand (MW)")
+           Data_Date = "Data Date", Hour_Number = "Hour Number",
+           Demand_Forecast = "Demand Forecast (MW)", Demand_MW = "Demand (MW)",
+           Net_Generation = "Net Generation (MW)", Total_Interchange = "Total Interchange (MW)",
+           Sum_Valid_DIBAs = "Sum(Valid DIBAs) (MW)", Demand_Adjusted = "Demand (MW) (Adjusted)",
+           Net_Generation_Adjusted = "Net Generation (MW) (Adjusted)",
+           Energy_Consumption = "Energy Consumption (megawatthours)")
   
   # Change type of pricing and date columns
   cleaned_df$Day_Ahead_Hourly_Price <- as.numeric(gsub("¢", "", cleaned_df$Day_Ahead_Hourly_Price))
@@ -38,6 +41,12 @@ data_cleaner <- function(data_directory, file_name = "FINAL_PJM_DATA.xlsx",
   
   # Add a year column
   cleaned_df$Year <- format(cleaned_df$Data_Date, "%Y")
+  
+  # Remove non-zero prices if the user desires
+  if (tolower(remove) == 'yes'){
+    cleaned_df <- cleaned_df %>%
+      filter(Real_Time_Hourly_Price != 0)
+  }
   
   # Separate data based on start date and COVID date
   pre_covid_df <- cleaned_df %>%
