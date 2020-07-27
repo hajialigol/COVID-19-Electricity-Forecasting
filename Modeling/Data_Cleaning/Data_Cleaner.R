59e90f6a82a077038ebfd191b538b17ac2421662
Daniel <danielhajialigol@gmail.com>
2020-07-27 16:23:23 -0400

    Added feature engineering function

--- Modeling/Data_Cleaning/Data_Cleaner.R
+++ Modeling/Data_Cleaning/Data_Cleaner.R
@@ -1,5 +1,7 @@
 # libraries
 require(dplyr)
+require(lubridate)
+require(fastDummies)
 
 # Desc:
   # This function returns two cleaned data sets for the "FINAL_PJM_DATA.xlsx" file.
@@ -10,8 +12,8 @@ require(dplyr)
 # Oupt:
   # df_list [list]: List of cleaned data frames before and after COVID-19.
 data_cleaner <- function(data_directory, file_name = "FINAL_PJM_DATA.xlsx",
-                         covid_date = "2020-03-11", beginning_date = "2019-01-01",
-                         remove = 'no'){
+                         covid_date = "2020-04-01", beginning_date = "2019-01-01",
+                         remove = 'no', feature_engineer = 'no'){
   
   # Set working directory to data directory
   setwd(data_directory)
@@ -56,10 +58,52 @@ data_cleaner <- function(data_directory, file_name = "FINAL_PJM_DATA.xlsx",
   covid_df <- cleaned_df %>%
     filter(Data_Date >= covid_date)
   
+  # Feature engineer based on Suraj's data
+  if (tolower(feature_engineer) == 'yes'){
+    pre_covid_df <- data_engineer(pre_covid_df)
+    covid_df <- data_engineer(covid_df)
+  }
+  
   # Create list of data frames with given names
   df_list <- list("pre-covid" = pre_covid_df, "covid" = covid_df)
   
   # Return data frames
   return(df_list)
 
-  }
+}
+
+
+# Desc:
+  # This function engineers features based on what Suraj's ideas.
+# Inpt:
+  # electricity_df [df]: data frame to add features to.
+# Oupt:
+  # electricity_df [df]: data frame with new features added to it.
+data_engineer <- function(electricity_df){
+  
+  # Initialize month feature
+  electricity_df[["Month"]] <- month(electricity_df[["Data_Date"]])
+  
+  # Subset data based on season type
+  electricity_df <- electricity_df %>%
+    filter(Month >= 0, Month <= 8)
+  
+  # Bin data frame based on season
+  electricity_df <- electricity_df %>%
+    mutate(Season = case_when(Month >= 0 & Month < 4 ~ "Winter",
+                              Month >= 4 & Month <= 8 ~ "Summer"))
+
+  # Find days of the week, remove Fridays, and make a column of weekdays vs weekends
+  electricity_df <- electricity_df %>%
+    mutate(Day_Of_Week = wday(Data_Date)) %>%
+    filter(Day_Of_Week != 5) %>%
+    mutate(Week_day = if_else(Day_Of_Week >= 6, "Weekend", "Weekday"))
+  
+  # Dummy encode variables
+  electricity_df <- electricity_df %>%
+    dummy_cols(select_columns = c("Week_day", "Season"))
+  
+  # Return updated dataframe
+  return (electricity_df)
+  
+}
